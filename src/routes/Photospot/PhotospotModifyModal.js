9b83f03825ac7c9d323a09b54684a94afed12b77
muja-code <pla2697@gmail.com>
2023-03-06 00:46:11 +0900

    fix: 포토스팟 수정 모달에 이전 포토스팟 데이터가 출력되는 버그 수정

--- src/routes/Photospot/PhotospotModifyModal.js
+++ src/routes/Photospot/PhotospotModifyModal.js
@@ -2,25 +2,22 @@ import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Button, Modal, Form, InputGroup } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
-import {
-  setModalName,
-  setShow,
-} from '../../store/photospot.slice';
+import { setShow } from '../../store/photospot.slice';
 import axios from 'axios';
 
 const PhotospotModifyModal = () => {
   let state = useSelector((state) => state);
   let dispatch = useDispatch();
   let navigate = useNavigate();
-  const [title, setTitle] = useState(state.photospotSlice.photospot.title);
-  const [description, setDescription] = useState(state.photospotSlice.photospot.description);
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
   const [imageFile, setImageFile] = useState([]);
 
   const handleClose = () => dispatch(setShow(false));
 
   return (
     <Modal
-      size="sm" show={state.photospotSlice.show} onHide={handleClose} centered>
+      size="sm" show={state.photospot.show} onHide={handleClose} centered>
       <Modal.Header closeButton>
         <Modal.Title>포토스팟 수정하기</Modal.Title>
       </Modal.Header>
@@ -28,11 +25,11 @@ const PhotospotModifyModal = () => {
         <Form>
           <Form.Group className="mb-3" controlId="formBasicTitle">
             <Form.Label>제목</Form.Label>
-            <Form.Control size="sm" type="text" value={title} placeholder="Title" onChange={(e) => {setTitle(e.target.value);}}/>
+            <Form.Control size="sm" type="text" defaultValue={state.photospot.data.title} placeholder="Title" onChange={(e) => {setTitle(e.target.value);}}/>
           </Form.Group>
           <Form.Group className="mb-3" controlId="formBasicDescription">
             <Form.Label>간단 설명</Form.Label>
-            <Form.Control as="textarea" rows={3} value={description} placeholder="Description" onChange={(e) => {setDescription(e.target.value);}}/>
+            <Form.Control as="textarea" rows={3} defaultValue={state.photospot.data.description} placeholder="Description" onChange={(e) => {setDescription(e.target.value);}}/>
           </Form.Group>
           <Form.Group className="mb-3" controlId="formBasicImageFile">
             <Form.Label>사진</Form.Label>
@@ -50,20 +47,27 @@ const PhotospotModifyModal = () => {
   );
 
   function modifyPhotospot() {
+    let modifyTitle = state.photospot.data.title;
+    let modifyDesc = state.photospot.data.description;
+
+    if (title) {
+      modifyTitle = title;
+    }
+
+    if (description) {
+      modifyDesc = description;
+    }
     const formData = new FormData();
 
-    formData.append('title', title);
-    formData.append('description', description);
+    formData.append('title', modifyTitle);
+    formData.append('description', modifyDesc);
     if (imageFile.length) {
       formData.append('image', imageFile[0]);
     }
 
     axios({
-      headers: {
-        'Content-Type': 'multipart/form-data',
-      },
       method: 'put',
-      url: `http://localhost:8080/api/collections/1/photospots/${state.photospotSlice.photospot.id}`,
+      url: `http://localhost:8080/api/collections/1/photospots/${state.photospot.data.id}`,
       data: formData,
       withCredentials: true,
     })
@@ -79,7 +83,7 @@ const PhotospotModifyModal = () => {
   function deletePhotospot() {
     axios({
       method: 'delete',
-      url: `http://localhost:8080/api/collections/1/photospots/${state.photospotSlice.photospot.id}`,
+      url: `http://localhost:8080/api/collections/1/photospots/${state.photospot.data.id}`,
       withCredentials: true,
     })
       .then(() => {
