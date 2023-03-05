93e69fed21933f3fb81311afc3b213d7e0b4f2c1
Park Jin <tstunas@gmail.com>
2023-03-05 19:10:53 +0900

    feat: redirectUri 경로로 라우팅되었을 때에 해당하는 로그인 페이지 보이게 하기

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import SampleList from './routes/sample/List';
 import MeetupsList from './routes/meetups/MeetupsList';
 import Header from './routes/components/Header';
 import NavSideBar from './routes/components/NavSideBar';
+import NaverLogin from './routes/oauth/NaverLogin';
+import KakaoLogin from './routes/oauth/KakaoLogin';
 
 function App() {
   return (
@@ -14,6 +16,8 @@ function App() {
         <Route path="/" element={<Main />}></Route>
         <Route path="/sample" element={<SampleList />}></Route>
         <Route path="/meetups" element={<MeetupsList />}></Route>
+        <Route path="/login/naver" element={<NaverLogin />}></Route>
+        <Route path="/login/kakao" element={<KakaoLogin />}></Route>
       </Routes>
     </div>
   );
