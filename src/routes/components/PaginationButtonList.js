eb3039bff769de5e4e73f03075e0774d00f9d19d
Park Jin <tstunas@gmail.com>
2023-03-08 21:31:10 +0900

    feat: 페이지네이션 버튼 리스트 완성

--- src/routes/components/PaginationButtonList.js
+++ src/routes/components/PaginationButtonList.js
@@ -1,42 +1,68 @@
 import { useEffect, useState } from "react";
 import Pagination from "react-bootstrap/Pagination";
 
-const PaginationButtonList = (current, total, changePage) => {
-  let [leftElipsis, setLeftElipsis] = useState();
-  let [rightElipsis, setRightElipsis] = useState();
-  let [pages, setPages] = useState();
+const PaginationButtonList = ({ current, total, changePage, itemPerPage }) => {
+  let [leftElipsis, setLeftElipsis] = useState(false);
+  let [rightElipsis, setRightElipsis] = useState(false);
+  let [pages, setPages] = useState([]);
+  let [end, setEnd] = useState(1);
 
   useEffect(() => {
-    setPages(
-      Array.from({ length: 5 }, (_, index) => index + current - 2).filter(
-        (page) => page > 1 && page < total
-      )
-    );
+    setEnd(Math.floor(total / itemPerPage) + 1);
+  }, [total])
+
+  useEffect(() => {
+    const pageList = Array.from(
+      { length: 5 },
+      (_, index) => index + current - 2
+    ).filter((page) => page > 1 && page < end);
+    setPages(pageList);
+
     setLeftElipsis(current - 2 > 2);
-    setRightElipsis(current + 3 < total);
-  }, [current, total]);
+    setRightElipsis(current + 3 < end);
+  }, [current, end]);
 
   return (
     <Pagination>
-      <Pagination.First onClick={() => changePage(1)}  disabled={current <= 1} />
-      <Pagination.Prev onClick={() => changePage(current - 1)}  disabled={current <= 1} />
+      <Pagination.First onClick={() => changePage(1)} disabled={current <= 1} />
+      <Pagination.Prev
+        onClick={() => changePage(current - 1)}
+        disabled={current <= 1}
+      />
       <Pagination.Item onClick={() => changePage(1)} active={current === 1}>
-        {1}
+        1
       </Pagination.Item>
       {leftElipsis ? <Pagination.Ellipsis /> : ""}
 
-      {pages.map((page) => (
-        <Pagination.Item onClick={() => changePage(page)} active={current === page}>
+      {pages.map((page, index) => (
+        <Pagination.Item
+          onClick={() => changePage(page)}
+          active={current === page}
+        >
           {page}
         </Pagination.Item>
       ))}
 
       {rightElipsis ? <Pagination.Ellipsis /> : ""}
-      <Pagination.Item onClick={() => changePage(total)} active={current === total}>
-        {total}
-      </Pagination.Item>
-      <Pagination.Next onClick={() => changePage(current + 1)} disabled={current >= total} />
-      <Pagination.Last onClick={() => changePage(total)} disabled={current >= total} />
+      {end > 1 ? (
+        <Pagination.Item
+          onClick={() => changePage(end)}
+          active={current === end}
+        >
+          {end}
+        </Pagination.Item>
+      ) : (
+        ""
+      )}
+
+      <Pagination.Next
+        onClick={() => changePage(current + 1)}
+        disabled={current >= end}
+      />
+      <Pagination.Last
+        onClick={() => changePage(end)}
+        disabled={current >= end}
+      />
     </Pagination>
   );
 };
