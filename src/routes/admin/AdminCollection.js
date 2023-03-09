5578090127c3d24a40fabe4636a5be1792203c96
Park Jin <tstunas@gmail.com>
2023-03-09 09:38:33 +0900

    feat: AdminAccount 복사하여 나머지 관리자 페이지 작성

--- src/routes/admin/AdminCollection.js
+++ src/routes/admin/AdminCollection.js
@@ -1,9 +1,94 @@
+import { useEffect, useState } from "react";
+import { Button } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import adminEnvironments from "../../environments/admin";
+import { setModalName, setShow } from "../../store/modal.slice";
+import apiAxios from "../../utils/api-axios";
+import PaginationButtonList from "../components/PaginationButtonList";
+import AdminTable from "./AdminTable";
+import AdminAccountDeleteButtons from "./components/AdminAccountDeleteButton";
+import AdminSearch from "./components/AdminSearch";
+import AdminCreateAccountModal from "./modals/AdminCreateAccountModal";
+
 const AdminCollection = () => {
+  const { ko: koName, getItemPath, header, width, transform, itemPerPage } =
+    adminEnvironments["collection"];
+
+  let [data, setData] = useState([]);
+  let [original, setOriginal] = useState([]);
+  let [keyword, setkeyword] = useState("");
+  let [page, setPage] = useState(1);
+  let [total, setTotal] = useState(1);
+
+  let state = useSelector((state) => state);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    goSearch();
+  }, [page]);
+
   return (
     <>
-      <h3>콜렉션 관리 페이지</h3>
+      {state.modal.modalName === "admin-signup" && <AdminCreateAccountModal />}
+
+      <h3>{koName} 관리</h3>
+      <AdminSearch
+        onClick={goSearch}
+        onChange={(e) => setkeyword(e.target.value)}
+      />
+      <AdminTable
+        header={header}
+        width={width}
+        data={data}
+        original={original}
+        done={done}
+        TableButtons={[AdminAccountDeleteButtons]}
+      />
+      <PaginationButtonList
+        current={page}
+        total={total}
+        itemPerPage={itemPerPage}
+        changePage={setPage}
+      />
+      <Button variant="primary" onClick={adminSignup}>
+        추가
+      </Button>
     </>
   );
+
+  function goSearch() {
+    apiAxios
+      .get(getItemPath, { params: { keyword, p: page } })
+      .then(({ status, data }) => {
+        const { data: items, total } = data;
+        const mappingData = items.map((item) =>
+          transform.map((fn) => fn(item))
+        );
+        setTotal(total);
+        setOriginal(items);
+        setData(mappingData);
+      })
+      .catch((err) => {
+        console.log(err);
+        alert("실패");
+      });
+  }
+
+  function done(order) {
+    if (!order) {
+      goSearch();
+    } else {
+      setData(data.filter((value, index) => index !== order));
+      setOriginal(original.filter((value, index) => index !== order));
+    }
+  }
+
+  function adminSignup() {
+    dispatch(setModalName("admin-signup"));
+    dispatch(setShow(true));
+    setPage(1);
+    goSearch();
+  }
 };
 
 export default AdminCollection;
