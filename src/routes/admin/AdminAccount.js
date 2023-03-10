166fa8c5b9bc561338ed1eb06986dc0f1dd7a079
boleesystem <boleesystem@gmail.com>
2023-03-10 15:37:20 +0900

    fix: 관리자 검색 쿼리 keyword를 search로 명칭 수정

--- src/routes/admin/AdminAccount.js
+++ src/routes/admin/AdminAccount.js
@@ -17,7 +17,7 @@ const AdminAccount = () => {
 
   let [data, setData] = useState([]);
   let [original, setOriginal] = useState([]);
-  let [keyword, setkeyword] = useState("");
+  let [search, setSearch] = useState("");
   let [page, setPage] = useState(1);
   let [total, setTotal] = useState(1);
 
@@ -36,7 +36,7 @@ const AdminAccount = () => {
       <h3>{koName} 관리</h3>
       <AdminSearch
         onClick={goSearch}
-        onChange={(e) => setkeyword(e.target.value)}
+        onChange={(e) => setSearch(e.target.value)}
       />
       <AdminTable
         header={header}
@@ -60,7 +60,7 @@ const AdminAccount = () => {
 
   function goSearch() {
     apiAxios
-      .get(getItemPath, { params: { keyword, p: page } })
+      .get(getItemPath, { params: { search, p: page } })
       .then(({ status, data }) => {
         const { data: items, total } = data;
         const mappingData = items.map((item) =>
