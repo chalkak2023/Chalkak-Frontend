0996db9bae6b82a3a586a5eb7d921528bd199b2b
Park Jin <tstunas@gmail.com>
2023-03-13 23:03:58 +0900

    feat: 소셜 로그인의 환경변수를 production과 development 환경을 다르게 하기
    - environments/social.js는 삭제

--- src/environments/social.js
+++
@@ -1,6 +0,0 @@
-export default {
-  naverClientId: 'nQfUzAQK5oaiS_ZzmoUe',
-  naverRedirectId: `${window.location.origin}/login/naver`,
-  kakaoClientId: '1a714019e35dcc3167e57afad9a04d74',
-  kakaoRedirectId: `${window.location.origin}/login/kakao`
-}
