f3bcb497bcdac677ee3e575e15ce13c074e50eaf
Park Jin <tstunas@gmail.com>
2023-03-15 19:41:30 +0900

    feat: 관리자 로그인하지 않으면 보이지 않게 하기

--- src/pages/AdminPage.js
+++ src/pages/AdminPage.js
@@ -1,10 +1,14 @@
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
 import AdminHeader from "../routes/components/AdminHeader";
 
 function AdminPage({ MainComponent }) {
+  const admin = useSelector((state) => state.admin);
+
   return (
     <>
       <AdminHeader />
-      <MainComponent />
+      {admin.loginState ? <MainComponent /> : <p>로그인이 필요합니다.</p>}
     </>
   );
 }
