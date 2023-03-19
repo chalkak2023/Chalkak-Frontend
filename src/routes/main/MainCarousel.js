ed0ee0dc1cd0a3dbf838a05243fda7615019b131
cchoseonghun <109499274+cchoseonghun@users.noreply.github.com>
2023-03-19 19:57:55 +0900

    fix: 변경된 backend api path 적용 및 photos가 아닌 setPhotos 로 되어있던 부분 수정

--- src/routes/main/MainCarousel.js
+++ src/routes/main/MainCarousel.js
@@ -14,17 +14,17 @@ const MainCarousel = () => {
       <Container style={{ marginBottom: '100px'}}>
         <h2 className='mt-5'>찰칵! 당신의 순간을 담을게요.</h2>
         <Carousel>
-        {
-          setPhotos.length > 0 ?
-          photos.map((photo, i) => (
-              <Carousel.Item key={i}>
-                <div className='imgBox' style={{ backgroundColor: 'transparent' }}>
-                  <img className="d-block w-100" src={photo.image} alt={i}/>
-                </div>
-              </Carousel.Item>
-          )) :
-          <h3>등록된 포토스팟이 없습니다.</h3>
-        }
+          {
+            photos.length > 0 ?
+            photos.map((photo, i) => (
+                <Carousel.Item key={i}>
+                  <div className='imgBox' style={{ backgroundColor: 'transparent' }}>
+                    <img className="d-block w-100" src={photo.image} alt={i}/>
+                  </div>
+                </Carousel.Item>
+            )) :
+            <h3>등록된 포토스팟이 없습니다.</h3>
+          }
         </Carousel>
       </Container>
     </>
@@ -32,7 +32,7 @@ const MainCarousel = () => {
 
   function getRandomPhoto() {
     apiAxios
-      .get(`/api/photospot/random`)
+      .get(`/api/photospots/random`)
       .then(({ status, data }) => {
         if (status === 200) {
           setPhotos(data);
