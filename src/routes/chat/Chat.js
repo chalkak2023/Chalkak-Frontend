a72335b9660aed7fc90e69df23ae98e267f65ec3
cchoseonghun <109499274+cchoseonghun@users.noreply.github.com>
2023-03-22 01:09:21 +0900

    feat: chat - 채팅 목록 api 연결 및 리스트에 세팅 추가

--- src/routes/chat/Chat.js
+++ src/routes/chat/Chat.js
@@ -1,13 +1,14 @@
+import './Chat.css';
 import { useCallback, useEffect, useRef, useState } from 'react';
 import { Container, Button } from 'react-bootstrap';
 import { useDispatch } from 'react-redux';
 import { setIsFooterOn } from '../../store/footer.slice';
 import { io } from 'socket.io-client';
-import './Chat.css';
+import apiAxios from '../../utils/api-axios';
 
 const socket = io(`${process.env.REACT_APP_SERVER_ADDRESS}/chat`);
 const ChatContainer = () => {
-  const [meetups, setMeetups] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+  const [meetups, setMeetups] = useState([]);
   const [temp, setTemp] = useState([1, 2, 3]);
 
   const [chats, setChats] = useState([]);
@@ -16,21 +17,14 @@ const ChatContainer = () => {
 
   let dispatch = useDispatch();
 
-  // 스크롤 제어
   useEffect(() => {
-    if (!chatContainerEl.current) return;
-
-    const chatContainer = chatContainerEl.current;
-    const { scrollHeight, clientHeight } = chatContainer;
-
-    if (scrollHeight > clientHeight) {
-      chatContainer.scrollTop = scrollHeight - clientHeight;
-    }
-  }, [chats.length]);
-
-  useEffect(() => {
+    // footer 제거
     dispatch(setIsFooterOn(false));
 
+    // 채팅방 목록 세팅
+    getMeetups();
+
+    // socket.io 메시지 핸들러 세팅
     const messageHandler = (chat) => {
       console.log('서버로부터 메시지 받음');
       console.log(`test: `);
@@ -45,6 +39,18 @@ const ChatContainer = () => {
       socket.off('message', messageHandler);
     };
   }, []);
+
+  // 스크롤 제어
+  useEffect(() => {
+    if (!chatContainerEl.current) return;
+
+    const chatContainer = chatContainerEl.current;
+    const { scrollHeight, clientHeight } = chatContainer;
+
+    if (scrollHeight > clientHeight) {
+      chatContainer.scrollTop = scrollHeight - clientHeight;
+    }
+  }, [chats.length]);
 
   const onChange = useCallback((e) => {
     setInputMessage(e.target.value);
@@ -68,14 +74,14 @@ const ChatContainer = () => {
           <h5 className="meetups_box_header">같이찍어요 채팅 목록</h5>
           <div className="meetups_box_body">
             {
-              meetups.map((x, i) => 
-                // <div className="meetups_box_body_item active" key={i}>
-                <div className="meetups_box_body_item" key={i}>
-                  <h5>모임 제목 <span>2023년 3월 20일</span></h5>
-                  <p>모임 내용</p>
+              meetups.length > 0 ?
+              meetups.map((meetup, i) => (
+                <div className="meetups_box_body_item" key={i} onClick={selectMeetup}>
+                  <h5>{meetup.title} <span>약속시간: {new Date(meetup.schedule).toLocaleString()}</span></h5>
+                  <p>{meetup.content}</p>
                   <Button className="chatExitBtn" variant="danger" size="sm" style={{ float: 'right' }} onClick={()=>{exitChat()}}>나가기</Button>
                 </div>
-              )
+              )) : ''
             }
           </div>
         </div>
@@ -84,7 +90,6 @@ const ChatContainer = () => {
         {/* 오른쪽 채팅 섹션 시작 */}
         <div className="chat_box">
           <div className="chat_history">
-            { meetups }
             { temp.map((x, i) => (
               <div key={i}>
                 <div className="incoming_msg">
@@ -114,6 +119,30 @@ const ChatContainer = () => {
       </Container>
     </>
   )
+
+  function selectMeetup(e) {
+    const clickedItem = e.currentTarget;
+    const items = document.querySelectorAll('.meetups_box_body_item');
+    items.forEach(item => {
+      if (item !== clickedItem) {
+        item.classList.remove('active');
+      }
+    });
+    clickedItem.classList.add('active');
+  }
+
+  function getMeetups() {
+    apiAxios
+      .get(`/api/chats`)
+      .then(({ status, data }) => {
+        if (status === 200) {
+          setMeetups(data);
+        }
+      }).catch((e) => {
+        console.log('axios 통신실패');
+        console.log(e);
+      });
+  }
 
   function exitChat() {
     alert('준비중');
