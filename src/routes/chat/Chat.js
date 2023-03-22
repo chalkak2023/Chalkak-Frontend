1ce00da4f16124c104c361c24e5f83551c7b3971
cchoseonghun <109499274+cchoseonghun@users.noreply.github.com>
2023-03-22 13:07:20 +0900

    feat: chat - socket.io를 통한 채팅 기능 구현

--- src/routes/chat/Chat.js
+++ src/routes/chat/Chat.js
@@ -1,7 +1,7 @@
 import './Chat.css';
-import { useCallback, useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { Container, Button } from 'react-bootstrap';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { setIsFooterOn } from '../../store/footer.slice';
 import { io } from 'socket.io-client';
 import apiAxios from '../../utils/api-axios';
@@ -9,12 +9,11 @@ import apiAxios from '../../utils/api-axios';
 const socket = io(`${process.env.REACT_APP_SERVER_ADDRESS}/chat`);
 const ChatContainer = () => {
   const [meetups, setMeetups] = useState([]);
-  const [temp, setTemp] = useState([1, 2, 3]);
-
   const [chats, setChats] = useState([]);
   const [inputMessage, setInputMessage] = useState('');
   const chatContainerEl = useRef(null);
 
+  let state = useSelector((state)=> state );
   let dispatch = useDispatch();
 
   useEffect(() => {
@@ -26,13 +25,13 @@ const ChatContainer = () => {
 
     // socket.io 메시지 핸들러 세팅
     const messageHandler = (chat) => {
-      console.log('서버로부터 메시지 받음');
-      console.log(`test: `);
-      console.log(chat);
-
+      setChats((prevChats) => [...prevChats, chat]);
+    }
+    const alertHandler = (chat) => {
       setChats((prevChats) => [...prevChats, chat]);
     }
     socket.on('message', messageHandler);
+    socket.on('alert', alertHandler);
     
     return () => {
       dispatch(setIsFooterOn(true));
@@ -40,7 +39,7 @@ const ChatContainer = () => {
     };
   }, []);
 
-  // 스크롤 제어
+  // 스크롤 제어 - 최신 메시지 따라 밑으로
   useEffect(() => {
     if (!chatContainerEl.current) return;
 
@@ -52,27 +51,14 @@ const ChatContainer = () => {
     }
   }, [chats.length]);
 
-  const onChange = useCallback((e) => {
-    setInputMessage(e.target.value);
-  }, []);
-
-  const onSendMessage = useCallback((e) => {
-    if (!inputMessage) return alert('메시지를 입력해 주세요.');
-
-    socket.emit('message', inputMessage, (chat) => {
-      console.log('서버로 메시지 보냄');
-      setChats((prevChats) => [...prevChats, chat]);
-      setInputMessage('');
-    });
-  }, [inputMessage]);
-
   return (
     <>
-      <Container className='chat_container' ref={chatContainerEl}>
+      <Container className='chat_container'>
         {/* 왼쪽 모임 목록 섹션 시작 */}
         <div className="meetups_box">
           <h5 className="meetups_box_header">같이찍어요 채팅 목록</h5>
           <div className="meetups_box_body">
+            {/* 채팅방 목록 */}
             {
               meetups.length > 0 ?
               meetups.map((meetup, i) => (
@@ -89,29 +75,48 @@ const ChatContainer = () => {
 
         {/* 오른쪽 채팅 섹션 시작 */}
         <div className="chat_box">
-          <div className="chat_history">
-            { temp.map((x, i) => (
-              <div key={i}>
-                <div className="incoming_msg">
-                  <div className='incoming_msg_writer'>닉네임1닉네임1: </div>
-                  <div className="incoming_msg_content">
-                    <p>테스트 채팅 1</p>
-                    <span className="msg_date">11:01 AM | 3월 20일</span>
-                  </div>
-                </div>
 
-                <div className="outgoing_msg">
-                  <div className="outgoing_msg_content">
-                    <p>테스트 채팅 2. 길게 보내면 어떻게 될까 테스트</p>
-                    <span className="msg_date">11:01 AM | 3월 20일</span>
-                  </div>
-                </div>
-              </div>
-            )) }
+          {/* 채팅창 시작 */}
+          <div className="chat_history" ref={chatContainerEl}>
+            { chats.map((chat, i) => {
+                if ('msgObj' in chat) {
+                  const msgObj = chat.msgObj;
+                  if (msgObj.userId === state.user.data.id) {
+                    return (
+                      <div className="outgoing_msg" key={i}>
+                        <div className="outgoing_msg_content">
+                          <p>{msgObj.message}</p>
+                          <span className="msg_date">{new Date(msgObj.createdAt).toLocaleString()}</span>
+                        </div>
+                      </div>
+                    )
+                  } else {
+                    return (
+                      <div className="incoming_msg" key={i}>
+                        <div className='incoming_msg_writer'>{msgObj.username}: </div>
+                        <div className="incoming_msg_content">
+                          <p>{msgObj.message}</p>
+                          <span className="msg_date">{new Date(msgObj.createdAt).toLocaleString()}</span>
+                        </div>
+                      </div>
+                    )
+                  }
+                } else if ('alert' in chat) {
+                  const alert = chat.alert;
+                  return (
+                    <div key={i}>알림: {alert.message}</div>
+                  )
+                }
+            }) }
           </div>
+          {/* 채팅창 끝 */}
 
           <div className="write_msg">
-            <input type="text" placeholder="메시지를 입력하세요" onChange={onChange} value={inputMessage} />
+            <input type="text" placeholder="메시지를 입력하세요" 
+              value={inputMessage} 
+              onChange={(e)=>{setInputMessage(e.target.value);}} 
+              // onKeyUp={pressEnterHandler}
+            />
             <Button className="chatSendBtn" size="sm" onClick={onSendMessage}>보내기</Button>
           </div>
         </div>
@@ -119,6 +124,31 @@ const ChatContainer = () => {
       </Container>
     </>
   )
+
+  // 엔터 입력 시 두 번 실행되는 문제로 일단 보류
+  // function pressEnterHandler(e) {
+  //   if (e.key === "Enter") {
+  //     onSendMessage();
+  //   }
+  // }
+
+  function onSendMessage() {
+    if (!inputMessage) {
+      return alert('메시지를 입력해 주세요.');
+    } else {
+      const msgObj = {
+        userId: state.user.data.id,
+        username: state.user.data.username,
+        message: inputMessage,
+        createdAt: new Date()
+      }
+      socket.emit('message', msgObj, (chat) => {
+        console.log('메시지 보냄');
+        setChats((prevChats) => [...prevChats, chat]);
+        setInputMessage('');
+      });
+    }
+  };
 
   function selectMeetup(e) {
     const clickedItem = e.currentTarget;
