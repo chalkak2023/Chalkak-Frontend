6e76bded2d6277d20e7e28bf185457733cd2da6b
cchoseonghun <109499274+cchoseonghun@users.noreply.github.com>
2023-03-22 15:39:39 +0900

    feat: chat - 채팅방 기능 추가

--- src/routes/chat/Chat.js
+++ src/routes/chat/Chat.js
@@ -11,6 +11,7 @@ const ChatContainer = () => {
   const [meetups, setMeetups] = useState([]);
   const [chats, setChats] = useState([]);
   const [inputMessage, setInputMessage] = useState('');
+  const [selectedRoom, setSelectedRoom] = useState(-1);
   const chatContainerEl = useRef(null);
 
   let state = useSelector((state)=> state );
@@ -62,7 +63,7 @@ const ChatContainer = () => {
             {
               meetups.length > 0 ?
               meetups.map((meetup, i) => (
-                <div className="meetups_box_body_item" key={i} onClick={selectMeetup}>
+                <div className="meetups_box_body_item" key={i} onClick={(e) => {selectRoom(e, meetup.id)}}>
                   <h5>{meetup.title} <span>약속시간: {new Date(meetup.schedule).toLocaleString()}</span></h5>
                   <p>{meetup.content}</p>
                   <Button className="chatExitBtn" variant="danger" size="sm" style={{ float: 'right' }} onClick={()=>{exitChat()}}>나가기</Button>
@@ -78,25 +79,26 @@ const ChatContainer = () => {
 
           {/* 채팅창 시작 */}
           <div className="chat_history" ref={chatContainerEl}>
-            { chats.map((chat, i) => {
-                if ('msgObj' in chat) {
-                  const msgObj = chat.msgObj;
-                  if (msgObj.userId === state.user.data.id) {
+            { selectedRoom > -1 ?
+              chats.map((chat, i) => {
+                if ('chatObj' in chat) {
+                  const chatObj = chat.chatObj;
+                  if (chatObj.userId === state.user.data.id) {
                     return (
                       <div className="outgoing_msg" key={i}>
                         <div className="outgoing_msg_content">
-                          <p>{msgObj.message}</p>
-                          <span className="msg_date">{new Date(msgObj.createdAt).toLocaleString()}</span>
+                          <p>{chatObj.message}</p>
+                          <span className="msg_date">{new Date(chatObj.createdAt).toLocaleString()}</span>
                         </div>
                       </div>
                     )
                   } else {
                     return (
                       <div className="incoming_msg" key={i}>
-                        <div className='incoming_msg_writer'>{msgObj.username}: </div>
+                        <div className='incoming_msg_writer'>{chatObj.username}: </div>
                         <div className="incoming_msg_content">
-                          <p>{msgObj.message}</p>
-                          <span className="msg_date">{new Date(msgObj.createdAt).toLocaleString()}</span>
+                          <p>{chatObj.message}</p>
+                          <span className="msg_date">{new Date(chatObj.createdAt).toLocaleString()}</span>
                         </div>
                       </div>
                     )
@@ -111,7 +113,8 @@ const ChatContainer = () => {
                     </div>
                   )
                 }
-            }) }
+              }) : ''
+            }
           </div>
           {/* 채팅창 끝 */}
 
@@ -120,8 +123,9 @@ const ChatContainer = () => {
               value={inputMessage} 
               onChange={(e)=>{setInputMessage(e.target.value);}} 
               // onKeyUp={pressEnterHandler}
+              disabled={selectedRoom < 0}
             />
-            <Button className="chatSendBtn" size="sm" onClick={onSendMessage}>보내기</Button>
+            <Button className="chatSendBtn" size="sm" onClick={sendMessage} disabled={selectedRoom < 0}>보내기</Button>
           </div>
         </div>
         {/* 오른쪽 채팅 섹션 끝 */}
@@ -132,38 +136,78 @@ const ChatContainer = () => {
   // 엔터 입력 시 두 번 실행되는 문제로 일단 보류
   // function pressEnterHandler(e) {
   //   if (e.key === "Enter") {
-  //     onSendMessage();
+  //     sendMessage();
   //   }
   // }
 
-  function onSendMessage() {
+  function sendMessage() {
     if (!inputMessage) {
       return alert('메시지를 입력해 주세요.');
-    } else {
-      const msgObj = {
-        userId: state.user.data.id,
-        username: state.user.data.username,
-        message: inputMessage,
-        createdAt: new Date()
-      }
-      socket.emit('message', msgObj, (chat) => {
-        console.log('메시지 보냄');
-        setChats((prevChats) => [...prevChats, chat]);
-        setInputMessage('');
-      });
     }
+    if (selectedRoom < 0) {
+      return alert('채팅방을 선택해야 합니다.');
+    }
+    const chatObj = {
+      roomId: selectedRoom,
+      userId: state.user.data.id,
+      username: state.user.data.username,
+      message: inputMessage,
+      createdAt: new Date()
+    }
+    socket.emit('message', chatObj, (chat) => {
+      setChats((prevChats) => [...prevChats, chat]);
+      setInputMessage('');
+    });
   };
 
-  function selectMeetup(e) {
+  function selectRoom(e, meetupId) {
     const clickedItem = e.currentTarget;
+    // 접속해있던 채팅방 퇴장
     const items = document.querySelectorAll('.meetups_box_body_item');
     items.forEach(item => {
       if (item !== clickedItem) {
         item.classList.remove('active');
       }
     });
+    if (selectedRoom > -1 && selectedRoom !== meetupId) {
+      leaveRoom(selectedRoom);
+    }
+
+    // 선택한 채팅방 접속
     clickedItem.classList.add('active');
+    setSelectedRoom(meetupId);
+    joinRoom(meetupId);
+
+    // 채팅창 초기화
+    setChats([]);
   }
+
+  function joinRoom(roomId) {
+    const chatObj = {
+      roomId,
+      userId: state.user.data.id,
+      username: state.user.data.username,
+    }
+    socket.emit('join-room', chatObj, (res) => {
+      if (!res.success) {
+        alert(`채팅방 접속 불가!`);
+        return console.log(`채팅방 접속 불가! :: ${res.payload}`);
+      }
+    });
+  }
+
+  function leaveRoom(roomId) {
+    const chatObj = {
+      roomId,
+      userId: state.user.data.id,
+      username: state.user.data.username,
+    }
+    socket.emit('leave-room', chatObj, (res) => {
+      if (!res.success) {
+        return console.log(`채팅방 퇴장 오류`);
+      }
+    });
+  };
 
   function getMeetups() {
     apiAxios
@@ -179,7 +223,11 @@ const ChatContainer = () => {
   }
 
   function exitChat() {
-    alert('준비중');
+    setSelectedRoom(-1);
+    setChats([]);
+    if (window.confirm(`정말 나가시겠습니까?\n나가면 채팅방에 다시 들어올 수 없습니다.`)) {
+      alert('준비중');
+    }
   }
 };
 
