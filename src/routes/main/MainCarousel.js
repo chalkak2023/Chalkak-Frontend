c335542242d31407ef3d4f8eded25c4e7863c398
muja-code <pla2697@gmail.com>
2023-03-23 16:04:01 +0900

    feat: 메인 이미지 슬라이드에서 이미지 클릭 시 해당 콜렉션으로 이동 추가

--- src/routes/main/MainCarousel.js
+++ src/routes/main/MainCarousel.js
@@ -1,10 +1,11 @@
 import { useEffect, useState } from 'react';
 import { Carousel, Container } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
 import apiAxios from '../../utils/api-axios';
 
 const MainCarousel = () => {
   const [photos, setPhotos] = useState([]);
-
+  const navigate = useNavigate();
   useEffect(() => {
     getRandomPhoto();
   }, []);
@@ -17,7 +18,7 @@ const MainCarousel = () => {
           {
             photos.length > 0 ?
             photos.map((photo, i) => (
-                <Carousel.Item key={i}>
+                <Carousel.Item key={i} onClick={() => {navigate(`/collection/${photo.photospot.collection.id}/photospot-view`)}}>
                   <div className='imgBox' style={{ backgroundColor: 'transparent' }}>
                     <img className="d-block w-100" src={photo.image} alt={i}/>
                   </div>
