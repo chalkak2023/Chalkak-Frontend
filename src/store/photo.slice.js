35ac9a6c879cf4ca0403713f39686e0d020133e3
muja-code <pla2697@gmail.com>
2023-03-24 00:38:42 +0900

    comment: photo의 data안에 어떤 데이터 들어가지는 주석 추가

--- src/store/photo.slice.js
+++ src/store/photo.slice.js
@@ -1,5 +1,12 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+
+/**
+ * data = {
+ *   userPhoto: 사용자가 클릭한 사진 정보
+ *   recommendPhoto: 사용자가 클릭한 사진에 대한 추천 사진정보 리스트
+ * }
+ */
 let photo = createSlice({
   name: 'photo',
   initialState: {
